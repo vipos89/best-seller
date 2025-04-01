c55eb5254c456c4765aa897aa73b5da4103b0312
vipos89 <1989146ViPoS>
2025-04-01 16:50:26 +0300

    add cache

--- app/Http/Controllers/Api/V1/BestSellerController.php
+++ app/Http/Controllers/Api/V1/BestSellerController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\V1\BestSellerRequest;
 use App\Services\BookService\Contracts\Service\BookServiceInterface;
 use App\Services\BookService\Dto\V3\Requests\BestSellerRequestDto;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class BestSellerController extends Controller
 {
@@ -17,7 +18,12 @@ class BestSellerController extends Controller
     public function getBestSellers(BestSellerRequest $request): JsonResponse
     {
         $dto = BestSellerRequestDto::fromArray($request->all());
-        $data = $this->bookService->getBestSellersHistory($dto);
+
+        $hashKey = sprintf('best_sellers_%s', md5(json_encode($dto, JSON_FORCE_OBJECT)));
+
+        $data = Cache::remember($hashKey, 600, function ()use ($dto){
+            return $this->bookService->getBestSellersHistory($dto);
+        });
 
         return response()->json($data);
     }
